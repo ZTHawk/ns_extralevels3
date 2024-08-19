2525c7597400a42d280205d739844e9f71fe2e51
Karol von Zmuda Trzebiatowski <zhawk83@gmail.com>
2024-08-19 07:20:58 +0200

    New offsets for NS 3.3 b9.

--- source/ns_const_advanced.h
+++ source/ns_const_advanced.h
@@ -2,7 +2,7 @@
 #define _NS_CONST_ADVANCED_H_
 
 
-#define OFFSET_WIN_WEAPON_ID		324	// 81 * 4	--->>> Check the weapon ID of the weapon ( eg: NS_WEAPON_PISTOL, NS_WEAPON_LMG, ... )
+#define OFFSET_WIN_WEAPON_ID		324	// 81 * 4	--->>> Check the weapon ID of the weapon ( eg: WEAPON_PISTOL, WEAPON_LMG, ... )
 #define OFFSET_LIN_WEAPON_ID		340	// (81+4) * 4	--->>> linux offset
 
 #define OFFSET_WIN_WEAPON_RELOADING	376	// 94 * 4	--->>> this offset is needed to check if weapon is reloading
@@ -11,20 +11,20 @@
 #define OFFSET_WIN_WEAPON_RELOAD_TIME	504	// 126 * 4	--->>> this offset is needed to check if player is reloading right now or now (allowed to fire his weapon)
 #define OFFSET_LIN_WEAPON_RELOAD_TIME	524	// (126+5) * 4	--->>> linux offset
 
-#define OFFSET_WIN_UNCLOAKTIME		1796	// 449 * 4	--->>> this offset sets the time when alien started uncloak (-1.0 when cloaked)
-#define OFFSET_LIN_UNCLOAKTIME		1716	// (449+5) * 4	--->>> defines the difference between win32 to linux
+#define OFFSET_WIN_UNCLOAKTIME		1800	// 449 * 4	--->>> this offset sets the time when alien started uncloak (-1.0 when cloaked)
+#define OFFSET_LIN_UNCLOAKTIME		1820	// (449+5) * 4	--->>> defines the difference between win32 to linux
 
-#define OFFSET_WIN_CLOAK		1800	// 450 * 4	--->>> this offset is needed to check if player is "cloaked" / "cloak fade in/out" right now or not (int = cloaked or not // float = sequence of cloak fade in/out)
-#define OFFSET_LIN_CLOAK		1820	// (450+5) * 4	--->>> defines the difference between win32 to linux
+#define OFFSET_WIN_CLOAK		1804	// 450 * 4	--->>> this offset is needed to check if player is "cloaked" / "cloak fade in/out" right now or not (int = cloaked or not // float = sequence of cloak fade in/out)
+#define OFFSET_LIN_CLOAK		1824	// (450+5) * 4	--->>> defines the difference between win32 to linux
 
-#define OFFSET_WIN_OLD_CLASS		6260	// 1565 * 4	--->>> this offset is needed to set players old class for gestate emulation
-#define OFFSET_LIN_OLD_CLASS		6280	// (1565+5) * 4 --->>> linux offset
+#define OFFSET_WIN_OLD_CLASS		6288	//6260	// 1565 * 4	--->>> this offset is needed to set players old class for gestate emulation
+#define OFFSET_LIN_OLD_CLASS		6356	//6280	// (1565+5) * 4 --->>> linux offset
 
-#define OFFSET_WIN_DIGESTING_PLAYER	6440	// 1610 * 4	--->>> this offset is needed to check which player is currently digested by the onos
-#define OFFSET_LIN_DIGESTING_PLAYER	6460	// (1610+5) * 4	--->>> linux offset
+#define OFFSET_WIN_DIGESTING_PLAYER	6492	//6440	// 1610 * 4	--->>> this offset is needed to check which player is currently digested by the onos
+#define OFFSET_LIN_DIGESTING_PLAYER	6560	//6460	// (1610+5) * 4	--->>> linux offset
 
-#define OFFSET_WIN_SOUND_EFFECTS	6452	// 1613 * 4	--->>> this offset is needed to check if the player has sound effects running (eg: when being digested) (at least setting to 0 it disables things like that)
-#define OFFSET_LIN_SOUND_EFFECTS	6472	// (1613+5) * 4	--->>> linux offset
+#define OFFSET_WIN_SOUND_EFFECTS	6504	//6452	// 1613 * 4	--->>> this offset is needed to check if the player has sound effects running (eg: when being digested) (at least setting to 0 it disables things like that)
+#define OFFSET_LIN_SOUND_EFFECTS	6572	//6472	// (1613+5) * 4	--->>> linux offset
 
 
 #ifdef __linux__
